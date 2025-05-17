88687d6d2a738ac299b60380ae68ea17d93e3ca8
George Quito <george-quitog@hotmail.com>
2025-05-17 15:01:00 -0600

    W02

--- controllers/users.js
+++ controllers/users.js
@@ -11,14 +11,53 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
+    const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
     });
 };
 
+const createUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ error: 'All fields (name, email, password) are required.' });
+    }
+
+    const newUser = { name, email, password };
+    const result = await mongodb.getDatabase().db().collection('users').insertOne(newUser);
+    res.status(201).json({ id: result.insertedId });
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ error: 'All fields (name, email, password) are required.' });
+    }
+
+    const updatedUser = { name, email, password };
+    const result = await mongodb.getDatabase().db().collection('users').updateOne(
+        { _id: userId },
+        { $set: updatedUser }
+    );
+
+    res.status(204).send(); // Success, no content returned
+};
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId });
+
+    res.status(204).send(); // Success, no content returned
+};
+
 module.exports = {
     getAll,
-    getSingle
-}
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
+};
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 
+// GET - all users
 router.get('/', usersController.getAll);
+
+// GET - single user by ID
 router.get('/:id', usersController.getSingle);
+
+// POST - Create new user
+router.post('/', usersController.createUser);
+
+// PUT - Update user by ID
+router.put('/:id', usersController.updateUser);
+
+// DELETE - Remove user by ID
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
