27c7a4330ba9a2a3d66045d1ecb29a6bb185d5a1
George Quito <george-quitog@hotmail.com>
2025-06-18 16:48:01 -0600

    W06

--- __tests__/items.test.js
+++ __tests__/items.test.js
@@ -1,12 +1,9 @@
 const request = require("supertest")
+const app = require("../app") 
 const mongoose = require("mongoose")
 
-let server
-
 beforeAll(async () => {
-  process.env.NODE_ENV = "test"
-  server = require("../server") 
-  await new Promise(resolve => setTimeout(resolve, 1000)) 
+  await new Promise(resolve => setTimeout(resolve, 1000))
 })
 
 afterAll(async () => {
@@ -15,7 +12,7 @@ afterAll(async () => {
 
 describe("GET /items", () => {
   it("should return a list of items", async () => {
-    const res = await request(server).get("/items")
+    const res = await request(app).get("/items")
     expect(res.statusCode).toBe(200)
     expect(Array.isArray(res.body)).toBe(true)
   }, 15000)
