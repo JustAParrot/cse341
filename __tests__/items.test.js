aba852f2d687e042116371ad85de2d177866da7e
George Quito <george-quitog@hotmail.com>
2025-06-18 16:58:02 -0600

    W06

--- __tests__/items.test.js
+++ __tests__/items.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest")
-const app = require("../server")
+const app = require("../app")
 const mongoose = require("mongoose")
 const Item = require("../models/itemModel")
 const Trainer = require("../models/trainerModel")
